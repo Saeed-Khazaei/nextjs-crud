f9116152147296b721b9ad334d9ce5d3ae2e70e7
Saeed-Khazaei <saeedkhazaei94@google.com>
2024-09-27 12:25:37 +0330

    Create Post endpoint to create new user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,6 +2,8 @@ import { IdWrapper } from "@/app/types/global.types";
 import { User } from "@/app/types/users.types";
 import { getDirectoryPath } from "@/app/utils/getDirectoryPath";
 import { parseJsonFile } from "@/app/utils/parseJsonFile";
+import { uuid4 } from "@/app/utils/uuid4";
+import fs from "fs";
 import { NextResponse } from "next/server";
 
 const USERS_DIRECTORY = getDirectoryPath("users.json");
@@ -11,4 +13,19 @@ const GET = async () => {
   return NextResponse.json(users);
 };
 
-export { GET };
+const POST = async (request: Request) => {
+  const body: User = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "bad request" }, { status: 403 });
+  const users = await parseJsonFile<IdWrapper<User>[]>(USERS_DIRECTORY);
+
+  const newUser = { ...body, id: uuid4() };
+
+  const modifiedUsers = [...users, newUser];
+
+  await fs.writeFileSync(USERS_DIRECTORY, JSON.stringify(modifiedUsers));
+
+  return NextResponse.json(newUser);
+};
+
+export { GET, POST };
