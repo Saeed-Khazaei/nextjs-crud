ac590b36f0529fccd380995f8956e19980aa13eb
Saeed-Khazaei <saeedkhazaei94@google.com>
2024-09-27 12:27:07 +0330

    Create Put endpoint to update a specific user by id

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,6 +2,7 @@ import { IdWrapper } from "@/app/types/global.types";
 import { User } from "@/app/types/users.types";
 import { getDirectoryPath } from "@/app/utils/getDirectoryPath";
 import { parseJsonFile } from "@/app/utils/parseJsonFile";
+import fs from "fs";
 import { NextResponse } from "next/server";
 
 const USERS_DIRECTORY = getDirectoryPath("users.json");
@@ -25,4 +26,26 @@ const GET = async (request: Request, context: { params: Params }) => {
   return NextResponse.json({ error: "not found" }, { status: 404 });
 };
 
-export { GET };
+const PUT = async (request: Request, context: { params: Params }) => {
+  const body = await request.json();
+  const { id } = context.params;
+
+  if (!id) {
+    return NextResponse.json({ error: "not found" }, { status: 404 });
+  }
+
+  const users = await parseJsonFile<IdWrapper<User>[]>(USERS_DIRECTORY);
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return NextResponse.json({ error: "not found" }, { status: 404 });
+  }
+  const modifiedUser = { ...body, id };
+  const modifiedUsers = users.filter((i) => i.id !== id).concat(modifiedUser);
+
+  await fs.writeFileSync(USERS_DIRECTORY, JSON.stringify(modifiedUsers));
+
+  return NextResponse.json(modifiedUser);
+};
+
+export { GET, PUT };
